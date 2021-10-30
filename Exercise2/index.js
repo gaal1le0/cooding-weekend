0ed6882cf739efe2d0dfc3de6dc71e23b5daed3f
Emanuel Martinez <me@manumartinez.es>
2021-10-30 12:02:32 +0200

    creada la funcion que limpia los valores

---
+++ Exercise2/index.js
@@ -0,0 +1,19 @@
+// declaración de datos iniciales
+let undefined;
+const sequence = [10, 4, 8, 27.4, 500, null, undefined, 100, 25, 40, 31, 20, 17, 1890, 13, 42];
+const letrasDNI = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+// limpiar los arrays
+const cleanData = (array) => {
+
+    let response = [];
+    // recorrer array
+    for (let value of array) {
+        if (typeof value === 'number') {
+            response.push(value);
+        }
+    }
+
+    return response;
+
+};
