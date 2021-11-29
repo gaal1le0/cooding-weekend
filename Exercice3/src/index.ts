1a576bf9dbb81fbbadadf40a2fcd58711a87a018
Manu <e-eeaz@gft.com>
2021-11-29 10:38:01 +0100

    fixed all data staff

--- Exercice3/src/index.ts
+++ Exercice3/src/index.ts
@@ -1,11 +1,11 @@
 class SortingObject implements Sorting {
 
     merge<T>(data: T[]): T[] {
-        
+        return []
     }
 
     bubbleSort<T>(data: T[]): T[] {
-        
+        return []
     }
 
 }
